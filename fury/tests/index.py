fec65c36a60a5bc9c77beccd7db561a6c3486b91
nyel-101 <dandevjr@gmail.com>
2025-02-14 23:38:07 +0000

    perf(auth): decouple middleware routes

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_445d97eeb97f4dc3a0ad3242a01c8772 = () => {
-  return '2026-02-03T06:44:32Z';
+const node_6eac0ca1d2574c38b40b43baddc0143c = () => {
+  return '2025-02-14T23:38:07Z';
 };
