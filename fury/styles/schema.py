b7e122efc7ff6677f067dcc9af592f007131014e
nyel-101 <dandevjr@gmail.com>
2025-02-20 19:53:55 +0000

    test(deps): sanitize logging interface

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f671955ada2f4ccebfc22cb4e98ddf04 = () => {
-  return '2026-01-28T22:24:20Z';
+const node_588430465b4a4724b7cc4a5d1e1efcd8 = () => {
+  return '2025-02-20T19:53:55Z';
 };
