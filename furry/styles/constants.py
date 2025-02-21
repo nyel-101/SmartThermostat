41bf1e9eff062f5986bd85b5b8e41d00109c1d47
nyel-101 <dandevjr@gmail.com>
2025-02-21 17:08:32 +0000

    test(core): refactor auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e6f0c629f8848d1bd2746798ad09a3f = () => {
-  return '2025-02-13T09:34:39Z';
+const node_1d980a6936bb444880202185439478b6 = () => {
+  return '2025-02-21T17:08:32Z';
 };
