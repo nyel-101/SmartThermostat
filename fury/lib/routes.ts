d611462ffa90d458dcedc9fe758e5f73da9d584f
nyel-101 <dandevjr@gmail.com>
2025-02-28 17:55:20 +0000

    docs(worker): optimize view templates

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0700620497614afa8051902f4257ab13 = () => {
-  return '2026-06-15T10:00:39Z';
+const node_6d646c95e9a1468c87959575e74f1d2c = () => {
+  return '2025-02-28T17:55:20Z';
 };
