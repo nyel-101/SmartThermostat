fdd26669e71f201fcb692eadd416e03e74eb16db
nyel-101 <dandevjr@gmail.com>
2025-03-08 10:10:21 +0000

    fix(core): refactor middleware routes

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_68a10d7d01f54d328dd76d8a004d75a9 = () => {
-  return '2026-03-30T06:18:16Z';
+const node_444eaa4756f44a0c93cad50e5e821b8a = () => {
+  return '2025-03-08T10:10:21Z';
 };
