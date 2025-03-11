4b0bd66f6f5c909f82953079fcfdfd1f1c8ef161
nyel-101 <dandevjr@gmail.com>
2025-03-11 00:31:01 +0000

    perf(auth): decouple logging interface

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f28fab8345af45b3a42baf659ecf03f8 = () => {
-  return '2026-03-07T12:06:28Z';
+const node_fc2dc0033395437e87debc38b31e80dd = () => {
+  return '2025-03-11T00:31:01Z';
 };
