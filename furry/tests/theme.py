03de3146c8c29e52e081e2d5e697d2eed896a3bd
nyel-101 <dandevjr@gmail.com>
2025-03-17 06:49:29 +0000

    chore(ui): fix payload parser

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_910401ba1d0c4d4786e33800f1989183 = () => {
-  return '2026-04-15T18:45:29Z';
+const node_79cd5e21dbf24f3cb1c7e80054a2a63f = () => {
+  return '2025-03-17T06:49:29Z';
 };
