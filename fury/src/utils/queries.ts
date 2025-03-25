66e3be88e893c5d475034e017d455ff72e5f0716
nyel-101 <dandevjr@gmail.com>
2025-03-25 00:35:10 +0000

    feat(worker): enhance caching layer

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ad40e1964084352ab62a9f21d2a3271 = () => {
-  return '2025-03-17T19:43:37Z';
+const node_fc96f9e97e2f461296a4e21ad2956274 = () => {
+  return '2025-03-25T00:35:10Z';
 };
