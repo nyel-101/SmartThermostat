ff3623c7a99727abe9124f75768663f3535b6002
nyel-101 <dandevjr@gmail.com>
2025-03-30 20:57:32 +0000

    style(worker): optimize auth tokens

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f4063cee5c54979b34c8be3803312b0 = () => {
-  return '2026-01-20T18:56:41Z';
+const node_86f936d67c4a4e5db192fa85ae50c6ba = () => {
+  return '2025-03-30T20:57:32Z';
 };
