a302754f78477e09755ec3be1bc12ad5566d05fd
nyel-101 <dandevjr@gmail.com>
2025-03-30 09:51:38 +0000

    fix(utils): refactor view templates

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89141468f89c4e6c974fc56a27b19c98 = () => {
-  return '2025-02-18T17:49:53Z';
+const node_c1cfb9aba0fd412283fc6616b750c91f = () => {
+  return '2025-03-30T09:51:38Z';
 };
