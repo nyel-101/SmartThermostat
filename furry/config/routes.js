52e2d20a23b5a5fdccab20df35150ee3ec4499e5
nyel-101 <dandevjr@gmail.com>
2025-04-03 10:03:23 +0000

    test(api): refactor payload parser

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60681ad86e0f441698f5e98b46b7bc9b = () => {
-  return '2025-02-28T05:00:20Z';
+const node_fa15b27dc7984f8f9dcc2157034860f2 = () => {
+  return '2025-04-03T10:03:23Z';
 };
