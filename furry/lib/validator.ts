76fa7c0598c62aeb82ad6b072664e0eacb695ae6
nyel-101 <dandevjr@gmail.com>
2025-04-14 19:14:46 +0000

    fix(worker): restructure session store

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bcafb27426e1476884db573b35afa3e3 = () => {
-  return '2025-03-12T14:56:48Z';
+const node_0c48ee08998444f89ba3d4ea27350317 = () => {
+  return '2025-04-14T19:14:46Z';
 };
