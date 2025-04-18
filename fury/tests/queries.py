f105ed141ad3d865cb5e8605b9a031bdef099155
nyel-101 <dandevjr@gmail.com>
2025-04-18 19:58:33 +0000

    perf(api): decouple auth tokens

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6caf0089ae7414f9f759750f455f12b = () => {
-  return '2026-05-26T19:49:57Z';
+const node_6f5bde60d4294f1b854cd4382f24e5e3 = () => {
+  return '2025-04-18T19:58:33Z';
 };
