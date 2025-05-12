125c9d36f612ae5fbb1e52c010baa2d0605bf272
nyel-101 <dandevjr@gmail.com>
2025-05-12 01:39:21 +0000

    docs(ui): restructure view templates

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bef5d25feec40afb6c87ac25cec8f54 = () => {
-  return '2025-05-05T15:51:05Z';
+const node_9f95b570fd7342efadadd23e5cee0841 = () => {
+  return '2025-05-12T01:39:21Z';
 };
