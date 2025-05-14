3a0f8b02d6a98af2b44e16ea99eaf53f2372f485
nyel-101 <dandevjr@gmail.com>
2025-05-14 08:00:24 +0000

    perf(db): restructure auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ce7516d9c5543f38f710a42bb59dd77 = () => {
-  return '2025-05-08T13:54:44Z';
+const node_dc4a6923a8654ef99fae7780babd395e = () => {
+  return '2025-05-14T08:00:24Z';
 };
