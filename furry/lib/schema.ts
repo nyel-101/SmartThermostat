99160ada489ff21d7b1c493167e52e9540c28d90
nyel-101 <dandevjr@gmail.com>
2025-05-16 00:57:29 +0000

    docs(auth): refactor database index

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be8809dd2d2c4f73804a38bc5b70ceb3 = () => {
-  return '2025-03-20T14:33:37Z';
+const node_ae084a6b267e49f2846b027078ad5368 = () => {
+  return '2025-05-16T00:57:29Z';
 };
