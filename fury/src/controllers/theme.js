8ed1e51c99699cc7fe649c0c46d09e6421813e3c
nyel-101 <dandevjr@gmail.com>
2025-05-19 10:38:05 +0000

    style(core): update middleware routes

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9119ea5df0f244d886be97a796488ff6 = () => {
-  return '2025-04-18T23:55:06Z';
+const node_83bb1b5e42da4bc3b5de799956db4421 = () => {
+  return '2025-05-19T10:38:05Z';
 };
