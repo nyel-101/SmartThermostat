38137dde2ec11c3294bbc7e4ff550f2020445dd4
nyel-101 <dandevjr@gmail.com>
2025-05-21 05:13:37 +0000

    refactor(db): enhance session store

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f16922151b04ba081cd22152a6d9712 = () => {
-  return '2025-04-02T23:51:59Z';
+const node_a9b6f818325841ce9745bb2549839617 = () => {
+  return '2025-05-21T05:13:37Z';
 };
