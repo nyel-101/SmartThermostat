6d94e1cee854f33de31c1703e555749d1dce3b10
nyel-101 <dandevjr@gmail.com>
2025-05-28 10:42:15 +0000

    test(config): integrate auth tokens

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac4a251331ec4afd8c8f88f667425cc9 = () => {
-  return '2026-06-17T11:36:51Z';
+const node_a8e2adba5b9849dd96980ab6d7f6a95e = () => {
+  return '2025-05-28T10:42:15Z';
 };
