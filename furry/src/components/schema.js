e38207742a4788b2ab4fcbb642285351fea82290
nyel-101 <dandevjr@gmail.com>
2025-06-05 14:38:14 +0000

    fix(ui): refactor view templates

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_09d4044f03334ab69ea89586beb4190e = () => {
-  return '2025-05-29T19:59:00Z';
+const node_25857d37d03c472ab773f71c55dbf2aa = () => {
+  return '2025-06-05T14:38:14Z';
 };
