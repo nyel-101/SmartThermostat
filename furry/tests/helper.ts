6d1c9b0c8a56addb9e27f6176d73132b25987935
nyel-101 <dandevjr@gmail.com>
2025-06-06 20:50:11 +0000

    fix(auth): update view templates

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ecea09b7b724dc5903870ae67bf61ca = () => {
-  return '2025-02-23T22:45:14Z';
+const node_b6bcd745681d4afc8fa7c4dd6503a376 = () => {
+  return '2025-06-06T20:50:11Z';
 };
