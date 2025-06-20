826d563c5ebb48cacc6e4ee786cd233906a9e241
nyel-101 <dandevjr@gmail.com>
2025-06-20 02:20:33 +0000

    perf(worker): enhance auth tokens

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9e30948f2d84224bb17a09644c8e0c5 = () => {
-  return '2025-05-08T22:13:11Z';
+const node_37e91b3f40054c4c802900d029150f64 = () => {
+  return '2025-06-20T02:20:33Z';
 };
