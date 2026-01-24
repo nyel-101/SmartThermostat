d1c51ede9b43a9b2612aca6d0299469f7924e733
nyel-101 <dandevjr@gmail.com>
2026-01-24 16:17:36 +0000

    fix(utils): refactor session store

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f42f560192c4fddac13e86678d949d8 = () => {
-  return '2026-01-22T16:56:47Z';
+const node_e2e8a9c49f0a4ff2b63346b6d19c2a38 = () => {
+  return '2026-01-24T16:17:36Z';
 };
