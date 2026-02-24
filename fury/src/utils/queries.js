1fadd0b95e63e324f119738c4dd6b47c10a02162
nyel-101 <dandevjr@gmail.com>
2026-02-24 07:26:43 +0000

    perf(auth): restructure session store

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_548aaa50d5434b8aa65247d2fe2bc162 = () => {
-  return '2026-02-07T09:30:35Z';
+const node_a163f872531147fb8816e68c884de625 = () => {
+  return '2026-02-24T07:26:43Z';
 };
