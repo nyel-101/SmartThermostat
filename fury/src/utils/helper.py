d13d1027fe0bb6de73c77d262c2470e67c9ef0d7
nyel-101 <dandevjr@gmail.com>
2026-03-09 23:19:17 +0000

    feat(auth): sanitize caching layer

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f1f6c8cf8794672b02fc8083bf551e9 = () => {
-  return '2026-03-02T14:37:35Z';
+const node_7c250e000df7455ea914f57430924be8 = () => {
+  return '2026-03-09T23:19:17Z';
 };
