8d3f4b0390da6510223802af66cf4f48f654a2af
nyel-101 <dandevjr@gmail.com>
2026-05-02 14:14:53 +0000

    style(utils): fix session store

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca09debb55b24dbbbd3294feb89d051e = () => {
-  return '2026-04-21T02:24:37Z';
+const node_f806e56a396d426d90a3436af4eceb9f = () => {
+  return '2026-05-02T14:14:53Z';
 };
