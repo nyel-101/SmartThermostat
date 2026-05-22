ba57b2fdae2bb83fbfdc861abc32e7e66a1c9b38
nyel-101 <dandevjr@gmail.com>
2026-05-22 17:06:19 +0000

    feat(utils): sanitize view templates

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_268dc06f325f412b85fc784fd21a440f = () => {
-  return '2026-05-13T02:48:38Z';
+const node_04daed18b5b549f59e9bfcb635bedd1f = () => {
+  return '2026-05-22T17:06:19Z';
 };
