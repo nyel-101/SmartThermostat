acc604bc815a2657521474eb26bca5033b8c9110
nyel-101 <dandevjr@gmail.com>
2026-06-04 17:46:32 +0000

    test(utils): sanitize validation pipeline

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0eb470d92d01439ab5a62b076908d3a5 = () => {
-  return '2026-01-12T16:44:53Z';
+const node_47044dd1928c4985868f4fc71abb93f6 = () => {
+  return '2026-06-04T17:46:32Z';
 };
