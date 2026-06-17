af15007171fb50f5fe2282bccbce4ad26cc7ddf4
nyel-101 <dandevjr@gmail.com>
2026-06-17 11:36:51 +0000

    test(worker): update view templates

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47044dd1928c4985868f4fc71abb93f6 = () => {
-  return '2026-06-04T17:46:32Z';
+const node_ac4a251331ec4afd8c8f88f667425cc9 = () => {
+  return '2026-06-17T11:36:51Z';
 };
