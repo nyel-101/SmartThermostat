c0c183b7941e891f6092aeb2362eabc6d0b1fc4e
nyel-101 <dandevjr@gmail.com>
2026-06-24 15:22:34 +0000

    fix(ui): refactor auth tokens

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d334c2ea09254b5ab87a021345a058d5 = () => {
-  return '2026-01-26T03:27:46Z';
+const node_dc96bfb9086f4b51a06a8cdebaeddab3 = () => {
+  return '2026-06-24T15:22:34Z';
 };
